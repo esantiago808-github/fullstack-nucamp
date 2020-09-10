19775306928380d23e6135e5925542343b4b549c
Edith <esantiago808@gmail.com>
2020-09-09 20:50:04 -1000

    Week 1 Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
-//middleware function
+app.use(morgan('dev'));
+
+//will automatically server index.html without specifying it if the file exists
+app.use(express.static(__dirname + '/public'));
+
+
+//middleware function - if a html file is not found in public, then it will default to this message
 app.use((req, res) => {
-    console.log(req.headers);
+    //console.log(req.headers); morgan will handle the request going forward
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
